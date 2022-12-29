b496f26d7e97395190b2dbad400da0934d4859f1
Varun Singhal <varun.singhal@salesforce.com>
2022-12-29 13:55:26 +0530

    Made Extractors constr private

--- src/main/java/com/varun/playground/spring/aop/services/Extractors.java
+++ src/main/java/com/varun/playground/spring/aop/services/Extractors.java
@@ -2,6 +2,8 @@ package com.varun.playground.spring.aop.services;
 
 public class Extractors {
 
+    private Extractors(){}
+
     public static final UserNameExtractor userNameExtractor = (user) -> user.getUserName();
 
 }
